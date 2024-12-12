5b44a5a4ff6860708d380234cd979d3e04c02912
Hamza Zafar <hamza.zafar@thehexatown.com>
2024-12-12 16:30:27 +0500

    reverted the code

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
-import express from 'express'
+import express from "express";
 
 const app = express();
 
-const PORT = 3001 ;
+const PORT = 3001;
 
-
-// app.listen(PORT , () =>{
-//     console.log(`App listening on PORT: ${PORT}`)
-// })
-
-
-
-app.listen(PORT , ()=>{
-    console.log(`App listening on PORT: ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`App listening on PORT: ${PORT}`);
+});

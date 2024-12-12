8972e1f884a9656ded370df78c8748d579f5cb8b
Hamza Zafar <hamza.zafar@thehexatown.com>
2024-12-12 18:51:59 +0500

    deleted the file

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ const PORT = 3001;
 //  server instance listening at port....
 //  server instance listening at port....
 
+// app.listen(PORT, () => {
+//   console.log(`App listening on PORT: ${PORT}`);
+// });
+
+
 app.listen(PORT, () => {
-  console.log(`App listening on PORT: ${PORT}`);
-});
+    console.log(`App listening on PORT: ${PORT}`);
+  });
+  
 

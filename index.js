c1a68ee54f18654b2b8f2c5142387651430b1d48
Hamza Zafar <hamza.zafar@thehexatown.com>
2024-12-13 11:03:41 +0500

    reverted the code

--- index.js
+++ index.js
@@ -9,4 +9,28 @@ app.listen(PORT, () => {
   console.log(`App listening on PORT: ${PORT}`);
 });
 
+import express from "express";
+
+const app = express();
+
+const PORT = 3001;
+
+
+app.listen(PORT, () => {
+  console.log(`App listening on PORT: ${PORT}`);
+});
+
+
+import express from "express";
+
+const app = express();
+
+const PORT = 3001;
+
+
+app.listen(PORT, () => {
+  console.log(`App listening on PORT: ${PORT}`);
+});
+
+
 
